0e391e0fb0f0b75a5cb35583e983893757a06de1
MarcAntoineAlex <18510756258@163.com>
2021-03-11 08:35:19 +0800

    k

--- run/h2e2/10.sh
+++ run/h2e2/10.sh
@@ -27,4 +27,4 @@ export MKL_THREADING_LAYER=GNU
 export CUDA_HOME=/usr/local/cuda-10.2
 # sugon does not support infiniband
 srun python ./augment.py --name a10 --dataset cifar10 --batch_size 196 --epochs 2000 --genotype \
-"Genotype(normal=[[('skip_connect', 0), ('skip_connect', 1)], [('max_pool_3x3', 0), ('dil_conv_5x5', 2)], [('max_pool_3x3', 0), ('dil_conv_5x5', 3)], [('sep_conv_5x5', 4), ('max_pool_3x3', 0)]], normal_concat=range(2, 6), reduce=[[('max_pool_3x3', 0), ('avg_pool_3x3', 1)], [('dil_conv_5x5', 2), ('avg_pool_3x3', 0)], [('dil_conv_5x5', 2), ('dil_conv_5x5', 3)], [('avg_pool_3x3', 0), ('dil_conv_5x5', 4)]], reduce_concat=range(2, 6))"
+"Genotype(normal=[[('sep_conv_3x3', 0), ('dil_conv_3x3', 1)], [('skip_connect', 0), ('skip_connect', 1)], [('skip_connect', 0), ('dil_conv_3x3', 3)], [('skip_connect', 0), ('dil_conv_3x3', 1)]], normal_concat=range(2, 6), reduce=[[('max_pool_3x3', 0), ('skip_connect', 1)], [('avg_pool_3x3', 0), ('skip_connect', 2)], [('avg_pool_3x3', 0), ('skip_connect', 2)], [('avg_pool_3x3', 0), ('skip_connect', 3)]], reduce_concat=range(2, 6))"
