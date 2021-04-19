bb71f4e620dd8b40f52826c2b3632b89fc7f6076
MarcAntoineAlex <18510756258@163.com>
2021-04-19 10:16:43 +0800

    k

--- run/200.sh
+++ run/200.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:V100:1
-#SBATCH -J w3-a200
+#SBATCH -J base-a200
 #SBATCH -o log/job-%j.log
 #SBATCH -e log/job-%j.err
 # shellcheck disable=SC2046
@@ -13,10 +13,10 @@ echo Directory is $PWD
 echo This job runs on the following nodes:
 echo "$SLURM_JOB_NODELIST"
 eval "$(conda shell.bash hook)"
-source /home/LAB/gaoch/miniconda2/bin/activate base
+source /home/LAB/anaconda3/bin/activate base
 # conda --version
 # which python
-conda activate gchmini
+conda activate cpy
 cd ..
 echo Python:
 which python
@@ -27,7 +27,8 @@ export MKL_THREADING_LAYER=GNU
 export CUDA_HOME=/usr/local/cuda-10.2
 # sugon does not support infiniband
 srun python ./augment.py --name a200 --dataset imagenet --batch_size 196 --epochs 2000 --genotype \
-"Genotype(normal=[[('dil_conv_5x5', 1), ('dil_conv_5x5', 0)], [('dil_conv_5x5', 2), ('sep_conv_3x3', 1)], [('dil_conv_5x5', 3), ('dil_conv_3x3', 2)], [('dil_conv_5x5', 2), ('dil_conv_5x5', 4)]], normal_concat=range(2, 6), reduce=[[('dil_conv_5x5', 0), ('dil_conv_3x3', 1)], [('dil_conv_5x5', 2), ('skip_connect', 0)], [('skip_connect', 2), ('skip_connect', 3)], [('dil_conv_5x5', 2), ('skip_connect', 4)]], reduce_concat=range(2, 6))"
+"Genotype(normal=[[('max_pool_3x3', 0), ('sep_conv_3x3', 1)], [('max_pool_3x3', 0), ('sep_conv_3x3', 1)], [('max_pool_3x3', 0), ('sep_conv_3x3', 1)], [('max_pool_3x3', 0), ('sep_conv_3x3', 2)]], normal_concat=range(2, 6), reduce=[[('max_pool_3x3', 0), ('skip_connect', 1)], [('max_pool_3x3', 0), ('dil_conv_5x5', 2)], [('sep_conv_5x5', 2), ('sep_conv_3x3', 3)], [('sep_conv_5x5', 4), ('dil_conv_5x5', 3)]], reduce_concat=range(2, 6))"
+# we2h3"Genotype(normal=[[('dil_conv_5x5', 1), ('dil_conv_5x5', 0)], [('dil_conv_5x5', 2), ('sep_conv_3x3', 1)], [('dil_conv_5x5', 3), ('dil_conv_3x3', 2)], [('dil_conv_5x5', 2), ('dil_conv_5x5', 4)]], normal_concat=range(2, 6), reduce=[[('dil_conv_5x5', 0), ('dil_conv_3x3', 1)], [('dil_conv_5x5', 2), ('skip_connect', 0)], [('skip_connect', 2), ('skip_connect', 3)], [('dil_conv_5x5', 2), ('skip_connect', 4)]], reduce_concat=range(2, 6))"
 # we2h2"Genotype(normal=[[('sep_conv_5x5', 1), ('dil_conv_5x5', 0)], [('sep_conv_3x3', 1), ('dil_conv_5x5', 2)], [('dil_conv_5x5', 3), ('dil_conv_3x3', 2)], [('dil_conv_5x5', 2), ('dil_conv_5x5', 4)]], normal_concat=range(2, 6), reduce=[[('dil_conv_5x5', 1), ('dil_conv_3x3', 0)], [('dil_conv_5x5', 1), ('dil_conv_5x5', 2)], [('skip_connect', 2), ('sep_conv_5x5', 3)], [('dil_conv_5x5', 2), ('skip_connect', 4)]], reduce_concat=range(2, 6))"
 # we2h1"Genotype(normal=[[('dil_conv_5x5', 0), ('dil_conv_5x5', 1)], [('dil_conv_5x5', 0), ('sep_conv_3x3', 1)], [('dil_conv_5x5', 1), ('dil_conv_3x3', 2)], [('dil_conv_5x5', 0), ('dil_conv_5x5', 4)]], normal_concat=range(2, 6), reduce=[[('max_pool_3x3', 0), ('dil_conv_3x3', 1)], [('skip_connect', 2), ('avg_pool_3x3', 0)], [('skip_connect', 2), ('avg_pool_3x3', 0)], [('skip_connect', 2), ('avg_pool_3x3', 0)]], reduce_concat=range(2, 6))"
 # h2e3"Genotype(normal=[[('sep_conv_5x5', 1), ('sep_conv_5x5', 0)], [('sep_conv_5x5', 2), ('sep_conv_5x5', 1)], [('dil_conv_5x5', 2), ('sep_conv_3x3', 3)], [('dil_conv_5x5', 4), ('dil_conv_5x5', 2)]], normal_concat=range(2, 6), reduce=[[('dil_conv_3x3', 0), ('sep_conv_5x5', 1)], [('dil_conv_5x5', 2), ('sep_conv_5x5', 1)], [('dil_conv_5x5', 2), ('dil_conv_5x5', 3)], [('dil_conv_5x5', 3), ('dil_conv_5x5', 2)]], reduce_concat=range(2, 6))"
